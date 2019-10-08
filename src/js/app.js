48032f83e0648a5bace4ca25a363c3d5d26919de
dsymonov <dsymonov@justcoded.co>
2019-10-08 13:01:08 +0300

    Clean project for start

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,9 @@
 // This file will be compiled into app.js and will not be minified.
 // Feel free with using ES6 here.
 
-import dotsEffect from './modules/dots';
-
 (($) => {
   // When DOM is ready
   $(() => {
-    dotsEffect.init();
+
   });
 })(jQuery);
