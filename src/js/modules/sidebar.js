576b0fa28f7b0ab35223ba714b7a571ae5f4e185
dsymonov <dsymonov@justcoded.co>
2019-10-09 15:13:38 +0300

    Make structure, add styles, make responsive

--- src/js/modules/sidebar.js
+++ src/js/modules/sidebar.js
@@ -2,6 +2,13 @@ const sidebar = () => {
   const $sidebar = $('.sidebar');
   const $btnSidebar = $('.header__btn-sidebar');
   const BTN_SIDEBAR_ACTIVE = 'header__btn-sidebar_is-active';
+  const $header = $('.header');
+  const $win = $(window);
+
+  const _sidebarPosition = () => {
+    $sidebar.height($win.height() - $header.outerHeight());
+    $sidebar.css({ top: $header.outerHeight() });
+  };
 
   const _addEventListeners = () => {
     $btnSidebar.on('click', function () {
@@ -12,10 +19,12 @@ const sidebar = () => {
         $(this).removeClass(BTN_SIDEBAR_ACTIVE);
       }
     });
+    $win.on('resize', _sidebarPosition);
   };
 
   const init = () => {
     _addEventListeners();
+    _sidebarPosition();
   };
 
   return {
