29ca8f30e725389ed583abb8cccac4dd9455aa39
dsymonov <dsymonov@justcoded.co>
2019-11-22 12:46:26 +0200

    Update WSK

--- src/vendor_entries/vendor.js
+++ src/vendor_entries/vendor.js
@@ -1,11 +1,15 @@
 // All imports in this file will be compiled into vendors.js file.
 //
-// Note: ES6 support for these imports is not supported in base build
-
-module.exports = [
-  './node_modules/jquery/dist/jquery.js',
-  './node_modules/echarts/dist/echarts.common.min.js',
-  './node_modules/air-datepicker/dist/js/datepicker.min.js',
-  './node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js',
-  './node_modules/@shopify/draggable/lib/es5/draggable.bundle.js'
-];
+// For import ES6+ with transpiling to ES5, put sources into 'es6' array
+module.exports = {
+  es5: [
+    './node_modules/jquery/dist/jquery.js',
+    './node_modules/echarts/dist/echarts.common.min.js',
+    './node_modules/air-datepicker/dist/js/datepicker.min.js',
+    './node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js',
+    './node_modules/@shopify/draggable/lib/es5/draggable.bundle.js'
+  ],
+  es6: [
+    // './node_modules/@shopify/draggable/lib/draggable.bundle.js',
+  ]
+};
